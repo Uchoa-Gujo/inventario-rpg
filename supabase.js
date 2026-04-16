84e482c453d707824bc955a0844fb9abefdbfc92
Uchoa-Gujo <luizguilhermeucha@gmail.com>
2026-04-16 00:47:17 -0300

    supabase.js

---
+++ supabase.js
@@ -0,0 +1,6 @@
+import { createClient } from 'https://esm.sh/@supabase/supabase-js@2'
+
+const supabaseUrl = 'https://njnvnefeygkndyhdnkqc.supabase.co'
+const supabaseKey = 'sb_publishable_eZvcp6DhCI_L_St_ROcCqw_CIWIxDop'
+
+export const supabase = createClient(supabaseUrl, supabaseKey)
